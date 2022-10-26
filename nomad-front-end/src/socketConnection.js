a3d650070af003288ddbc024be592bf81aa9bf29
tomlebl <tl12@st-andrews.ac.uk>
2022-10-26 11:42:26 +0100

    fix: frontend socket connection

--- nomad-front-end/src/socketConnection.js
+++ nomad-front-end/src/socketConnection.js
@@ -1,5 +1,7 @@
 import { io } from 'socket.io-client'
 
-const socket = io(process.env.REACT_APP_API_URL)
+const socket = io(
+  process.env.NODE_ENV === 'production' ? window.location.hostname : process.env.REACT_APP_API_URL
+)
 
 export default socket
