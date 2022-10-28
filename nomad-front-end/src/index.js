f3b59a80cc51d7b2d264bf69a8cd69efd3bd00d8
tomlebl <tl12@st-andrews.ac.uk>
2022-10-28 12:34:42 +0100

    React 18 root.render

--- nomad-front-end/src/index.js
+++ nomad-front-end/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import * as serviceWorker from './serviceWorker'
 import { BrowserRouter } from 'react-router-dom'
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
@@ -48,13 +48,14 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
-ReactDOM.render(
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 )
 
 // If you want your app to work offline and load faster, you can change
