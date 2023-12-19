c3e2c32edc41d724333d6877c646c355a18955e3
tomlebl <tl12@st-andrews.ac.uk>
2023-12-19 16:52:25 +0000

    improved datastore stats

--- nomad-rest-api/controllers/admin/stats.js
+++ nomad-rest-api/controllers/admin/stats.js
@@ -1,9 +1,25 @@
 import User from '../../models/user.js'
+import Group from '../../models/group.js'
 
 export async function getDatastoreStats(req, res) {
-  const users = await User.find({ isActive: true })
+  const { user, group } = req.query
+  let searchParams = {}
+  let respObj = {}
 
-  const statsObj = users.reduce(
+  if (user && !group) {
+    searchParams = { username: user }
+    respObj = { user }
+  } else if (group && !user) {
+    const { _id } = await Group.findOne({ groupName: group })
+    searchParams = { group: _id }
+    respObj = { group }
+  } else {
+    searchParams = { isActive: true }
+  }
+
+  const users = await User.find(searchParams)
+
+  const { nmriumCount, downloadCount } = users.reduce(
     (accuObj, user) => {
       accuObj.nmriumCount += user.stats.nmriumCount
       accuObj.downloadCount += user.stats.downloadCount
@@ -12,5 +28,7 @@ export async function getDatastoreStats(req, res) {
     { nmriumCount: 0, downloadCount: 0 }
   )
 
-  res.status(200).json(statsObj)
+  respObj.nmriumUsage = `${Math.round((nmriumCount / (nmriumCount + downloadCount)) * 100)}%`
+
+  res.status(200).json(respObj)
 }
