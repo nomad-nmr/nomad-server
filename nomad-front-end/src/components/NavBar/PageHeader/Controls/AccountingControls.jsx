a6ae201a464d2ebbdd1cf81f5c3147adfb258c41
tomlebl <tl12@st-andrews.ac.uk>
2024-09-13 14:50:13 +0100

    fixing CSV download

--- nomad-front-end/src/components/NavBar/PageHeader/Controls/AccountingControls.jsx
+++ nomad-front-end/src/components/NavBar/PageHeader/Controls/AccountingControls.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Button, Space, Divider } from 'antd'
 import classes from '../PageHeader.module.css'
-import { CSVLink } from "react-csv";
+import { CSVLink } from 'react-csv'
+import dayjs from 'dayjs'
 import { CloudDownloadOutlined } from '@ant-design/icons'
 
-
 const AccountingControls = props => {
-  const { setGrantsVisible,  tableData, tableHeader, accType } = props
+  const { setGrantsVisible, tableData, tableHeader, accType } = props
   const standardColumns = {
     grants: ['Grant Code', 'Description', 'Users', 'Manual Cost', 'Auto Cost', 'Total Cost [£]']
   }
@@ -14,59 +14,46 @@ const AccountingControls = props => {
     let columns = []
 
     if (type === 'Grants') {
-
       columns = standardColumns.grants
-
     } else {
-
       columns = [head]
-      let presentColumns = data[0].costsPerInstrument;
+      let presentColumns = data[0].costsPerInstrument
       presentColumns.forEach(({ instrument }) => {
         const newColumnsToAdd = [
-          instrument + " Exp Time Manual",
-          instrument + " Exp Time Auto",
-          instrument + " Exp Time Cost [£]"
+          instrument + ' Exp Time Manual',
+          instrument + ' Exp Time Auto',
+          instrument + ' Exp Time Cost [£]'
         ]
         columns = [...columns, ...newColumnsToAdd]
-
-      });
+      })
       columns = [...columns, 'Total Cost [£]']
     }
 
-
-
-    return columns;
+    return columns
   }
-
-
 
   const rowsParser = (data, type) => {
     //column names are done, now do the data
-    let rows = [];
+    let rows = []
 
     if (type === 'Grants') {
-
       data.forEach(row => {
-        let flatrow = [];
+        let flatrow = []
         //destructure important stuff
-        const { costExps, costClaims, grantCode, description, totalCost, usersArray } = row;
+        const { costExps, costClaims, grantCode, description, totalCost, usersArray } = row
 
         //flatten the users array as text
-        let users = usersArray.map(userOBJ => (
-          `${userOBJ.username} (${userOBJ.fullName})`
-        ))
+        let users = usersArray.map(userOBJ => `${userOBJ.username} (${userOBJ.fullName})`)
 
         //finalize
-        flatrow = [grantCode, description, users.join(', '), costClaims, costExps, totalCost];
+        flatrow = [grantCode, description, users.join(', '), costClaims, costExps, totalCost]
 
         //add to the rows
         rows = [...rows, flatrow]
-      });
-
+      })
     } else {
-
       data.forEach(row => {
-        let FlatRow = [row.name];
+        let FlatRow = [row.name]
         row.costsPerInstrument.forEach(instrumentData => {
           const { cost, expTimeAuto, expTimeClaims } = instrumentData
           FlatRow = [...FlatRow, expTimeClaims, expTimeAuto, cost]
@@ -81,10 +68,8 @@ const AccountingControls = props => {
   }
 
   const dataParser = (head, data, type) => {
-
     let rows = data[1] ? [columnsParser(head, data, type), ...rowsParser(data, type)] : []
-    return rows;
-
+    return rows
   }
 
   return (
@@ -101,13 +86,18 @@ const AccountingControls = props => {
           Add Grant
         </Button>
       )}
-      <Divider type="vertical" />
+      <Divider type='vertical' />
       <CSVLink
         aria-disabled={!tableData[1]}
         data={dataParser(tableHeader, tableData, accType)}
-      ><CloudDownloadOutlined /></CSVLink>
+        filename={`${accType} Accounting ${dayjs().format('DD-MM-YY HH_mm')}.csv`}
+      >
+        <Button icon={<CloudDownloadOutlined />}>Download CSV</Button>
+      </CSVLink>
     </Space>
   )
 }
 
 export default AccountingControls
+
+// <CloudDownloadOutlined style={{ fontSize: '20px' }} />
--- nomad-front-end/vite.config.js
+++ nomad-front-end/vite.config.js
@@ -7,8 +7,5 @@ export default defineConfig({
   server: {
     host: true,
     port: 3003
-  },
-  optimizeDeps: {
-    exclude: ['react-csv']
   }
 })
