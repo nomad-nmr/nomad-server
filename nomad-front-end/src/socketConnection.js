0d5fac60cfe1923b642e3ddc775153b2033ee21c
tomlebl <tl12@st-andrews.ac.uk>
2024-10-30 14:49:45 +0000

    sockets url fix

--- nomad-front-end/src/socketConnection.js
+++ nomad-front-end/src/socketConnection.js
@@ -1,5 +1,7 @@
 import { io } from 'socket.io-client'
 
-const socket = io(import.meta.env.PROD ? window.location.hostname : import.meta.env.VITE_API_URL)
+const socket = io(
+  import.meta.env.PROD ? window.location.hostname : import.meta.env.VITE_SOCKETS_URL
+)
 
 export default socket
