9aa7de15d68b51595ca6dd1516571b142aaaeea3
tomlebl <tl12@st-andrews.ac.uk>
2025-01-14 12:02:24 +0000

    user-account tests added

---
+++ nomad-rest-api/tests/user-account.test.js
@@ -0,0 +1,64 @@
+import { it, expect, describe, beforeEach, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import mongoose from 'mongoose'
+
+import app from '../app.js'
+import User from '../models/user.js'
+
+import { connectDB, dropDB, setupDB } from './fixtures/db.js'
+import { testUserOne } from './fixtures/data/users.js'
+
+beforeAll(connectDB)
+afterAll(dropDB)
+beforeEach(setupDB)
+
+describe('GET /api/user-account/settings', () => {
+  it('should fail with status code 403 if user is not authorised', async () => {
+    await request(app).get('/api/user-account/settings').expect(403)
+  })
+
+  it('should return object with authorised user ', async () => {
+    const { body } = await request(app)
+      .get('/api/user-account/settings')
+      .set('Authorization', `Bearer ${testUserOne.tokens[0].token}`)
+      .expect(200)
+
+    expect(body).toMatchObject({
+      username: testUserOne.username,
+      fullName: testUserOne.fullName,
+      sendStatusError: true,
+      sendStatusArchived: true
+    })
+  })
+})
+
+describe('PATCH /api/user-account/settings', () => {
+  it('should fail with status code 403 if user is not authorised', async () => {
+    await request(app).patch('/api/user-account/settings').expect(403)
+  })
+
+  it('should update account settings for authorised user', async () => {
+    const { body } = await request(app)
+      .patch('/api/user-account/settings')
+      .send({
+        fullName: 'New Name',
+        sendStatusError: false,
+        sendStatusArchived: false
+      })
+      .set('Authorization', `Bearer ${testUserOne.tokens[0].token}`)
+      .expect(200)
+
+    expect(body).toMatchObject({
+      fullName: 'New Name',
+      sendStatusError: false,
+      sendStatusArchived: false
+    })
+
+    //assessing  change in DB
+
+    const user = await User.findById(testUserOne._id)
+
+    expect(user).toHaveProperty('fullName', 'New Name')
+    expect(user.sendStatusEmail).toMatchObject({ error: false, archived: false })
+  })
+})
