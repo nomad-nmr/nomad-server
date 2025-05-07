eae52caa0206364cc3fdea66b4cfeef059e0c339
tomlebl <tl12@st-andrews.ac.uk>
2025-05-07 10:56:52 +0100

    return oldEntry tracker fix

--- nomad-rest-api/controllers/tracker/updateStatusFromHist.js
+++ nomad-rest-api/controllers/tracker/updateStatusFromHist.js
@@ -22,14 +22,15 @@ const updateStatusFromHist = async (instrument, statusTable, historyTable) => {
           !oldEntry ||
           oldEntry.status !== entry.status
         ) {
+          //avoiding to update status to "Available" if experiment is canceled through IconNMR
+          //this is to avoid disappearing of archived experiments from the experiment search
+          if (oldEntry && oldEntry.status !== 'Available' && entry.status === 'Available') {
+            return oldEntry
+          }
+
           const historyTableItem = historyTable.find(
             i => i.datasetName === entry.datasetName && i.expNo === entry.expNo
           )
-
-          //avoiding to updated status to "Available" if experiment is canceled through IconNMR
-          if (oldEntry && oldEntry.status !== 'Available' && entry.status === 'Available') {
-            return
-          }
 
           const updateObj = {
             status: entry.status,
@@ -46,37 +47,43 @@ const updateStatusFromHist = async (instrument, statusTable, historyTable) => {
 
           const { datasetName, expNo, group } = entry
 
-          if (oldEntry) {
-            if (oldEntry.status === 'Available') {
-              updateObj.submittedAt = new Date()
-            }
-
-            if (oldEntry.status === 'Running' && entry.status === 'Completed') {
-              //sending message to client through socket to upload data
-              if (process.env.DATASTORE_ON !== 'false') {
-                sendUploadCmd(
-                  instrument._id.toString(),
-                  { datasetName, expNo, group },
-                  'upload-auto'
-                )
-              }
-            }
-
-            if (oldEntry.status === 'Running' && entry.status === 'Error') {
-              sendStatusEmail.error(datasetName)
-            }
-
-            if (oldEntry.status !== 'Running' && entry.status === 'Running')
-              updateObj.runningAt = new Date()
-          } else if (entry.status === 'Available' && expNo === '10') {
-            // sending pending status email for first experiment of dataset/holder
-            sendStatusEmail.pending(datasetName, instrument._id)
-          }
-          const expHistEntry = await Experiment.findOneAndUpdate({ expId }, updateObj)
+          const expHistEntry = await Experiment.findOne({ expId })
 
           if (expHistEntry) {
+            if (oldEntry) {
+              if (oldEntry.status === 'Available') {
+                updateObj.submittedAt = new Date()
+              }
+
+              if (oldEntry.status === 'Running' && entry.status === 'Completed') {
+                //sending message to client through socket to upload data
+                if (process.env.DATASTORE_ON !== 'false') {
+                  sendUploadCmd(
+                    instrument._id.toString(),
+                    { datasetName, expNo, group },
+                    'upload-auto'
+                  )
+                }
+              }
+
+              if (oldEntry.status === 'Running' && entry.status === 'Error') {
+                sendStatusEmail.error(datasetName)
+              }
+
+              if (oldEntry.status !== 'Running' && entry.status === 'Running')
+                updateObj.runningAt = new Date()
+            } else if (entry.status === 'Available' && expNo === '10') {
+              // sending pending status email for first experiment of dataset/holder
+              sendStatusEmail.pending(datasetName, instrument._id)
+            }
+
+            const expHistUpdated = { ...expHistEntry, ...updateObj }
+
+            await expHistUpdated.save()
+
             const { solvent, parameters, night, priority, submittedAt, updatedAt, batchSubmit } =
               expHistEntry
+
             return {
               ...entry,
               solvent,
