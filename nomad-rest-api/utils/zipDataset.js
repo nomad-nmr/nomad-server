0eeda62b74bffcc4c23c1c24ba7c73a4eabc3f74
tomlebl <tl12@st-andrews.ac.uk>
2025-10-14 17:15:26 +0100

    sampleManager files added to download zip

--- nomad-rest-api/utils/zipDataset.js
+++ nomad-rest-api/utils/zipDataset.js
@@ -2,6 +2,7 @@ import path from 'path'
 import fs from 'fs/promises'
 
 import JSZip from 'jszip'
+import moment from 'moment'
 
 import Dataset from '../models/dataset.js'
 import Experiment from '../models/experiment.js'
@@ -29,6 +30,7 @@ const zipDataset = async (jszipfile, datasetId) => {
             experiment.dataPath,
             experiment.expId + '.zip'
           )
+
           const zipFile = await fs.readFile(zipFilePath)
           const zipObject = await JSZip.loadAsync(zipFile)
 
@@ -36,7 +38,6 @@ const zipDataset = async (jszipfile, datasetId) => {
 
           //Changing subfolder structure in the zip file
           Object.keys(zipObject.files).forEach(key => {
-            console.log(key)
             let newKey
             if (key.split('/').length === 1) {
               newKey = sanitisedTitle + '/'
@@ -47,8 +48,11 @@ const zipDataset = async (jszipfile, datasetId) => {
               )
             }
 
-            zipObject.files[newKey] = zipObject.files[key]
-            delete zipObject.files[key]
+            //if subfolder structure has changed, rename the key
+            if (newKey !== key) {
+              zipObject.files[newKey] = zipObject.files[key]
+              delete zipObject.files[key]
+            }
           })
 
           const zipContent = await zipObject.generateAsync({ type: 'nodebuffer' })
@@ -60,10 +64,22 @@ const zipDataset = async (jszipfile, datasetId) => {
       jszipfile.file(sanitisedTitle + '/' + i.label + '.mol', i.molfile)
     })
 
+    if (dataset.sampleManagerData.length > 0) {
+      dataset.sampleManagerData.forEach(i => {
+        const jsonFileName = timestampToFilename(i.Metadata.created_timestamp, i.Sample.Label)
+        jszipfile.file(sanitisedTitle + '/' + jsonFileName, JSON.stringify(i, null, 2))
+      })
+    }
+
     return Promise.resolve()
   } catch (error) {
     return Promise.reject(error)
   }
+}
+
+function timestampToFilename(ts, label) {
+  const formatted = moment.utc(ts).local().format('YYYY-MM-DD_HHmmss')
+  return `${formatted}_${label}.json`
 }
 
 export default zipDataset
