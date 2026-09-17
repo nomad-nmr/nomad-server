ac1efd60ecac8721e3e57aace7d616f2a1d50553
tomlebl <tl12@st-andrews.ac.uk>
2026-09-17 15:12:58 +0100

    fixing Sample Manager link - v3.8.0 ready

--- nomad-front-end/src/components/NavBar/MainMenu/MainMenu.jsx
+++ nomad-front-end/src/components/NavBar/MainMenu/MainMenu.jsx
@@ -90,7 +90,7 @@ const MainMenu = props => {
       icon: <FormOutlined style={{ fontSize: 20 }} />,
       label: (
         <a
-          href='https://nmr-samples.github.io/online/'
+          href='https://nmrsamples.github.io/online/'
           target='_blank'
           rel='noopener noreferrer'
           style={{ fontSize: '16px', fontWeight: 500, color: 'inherit' }}
